ed4749603c7442aadf243d9ab22c7c8a4d209e1b
akaldhaheri <akaldhaheri@mail.csuchico.edu>
2018-02-27 14:30:06 -0800

    vectors arrays

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ int main()
 	cout<<"==ITEMS TO BUY==";
 	for(int numItems=0; numItems<5; numItems++)
     	{
-		cout<<endl<<numItems+1;
+		cout<<endl<<numItems+1 << " ";
 
 		if(list[numItems]!="")
 	    	{
